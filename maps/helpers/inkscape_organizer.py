e52cac5674ff6347c5ce9b95c222c97da6211ac6
ehhhahah <ehhhahah@gmail.com>
2022-06-21 19:34:36 +0200

    page look and map done

---
+++ maps/helpers/inkscape_organizer.py
@@ -0,0 +1,26 @@
+sound_num = 1
+# all_sounds = 11  # counting from zero
+
+color_match = 'style="fill:#'
+color_main = '#ff0000'
+
+new_file_str = ""
+
+with open('maps/mapa_inkscaped.svg') as map_file:
+    for line in map_file:
+        # TODO line = line.replace('fill:#000000', "")
+        if "<g" in line:
+            line = line.replace(
+                '<g',
+                f"""<g class="puzzle piece{sound_num}" onclick="playSound({sound_num})" """
+                )
+            sound_num += 1
+        if color_match in line:
+            # TODO
+            pass
+        new_file_str += line
+
+print(f"Generated {sound_num} pieces")
+
+with open('maps/helpers/new_map_parsed.svg', 'w') as f:
+    f.write(new_file_str)
