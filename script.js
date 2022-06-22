9856286e2b7bf22f7f26de29604d1b1c64ca3013
ehhhahah <ehhhahah@gmail.com>
2022-06-22 23:49:16 +0200

    major update of page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let ALL_AUDIOS = new Array(2)
+let SCROLL_POSITION = 0
 
 function hideShowClassElement(className) {
     let currState = document.getElementsByClassName(`piece${className}`)[0].classList.toggle(`piece-active`);
@@ -6,18 +7,14 @@ function hideShowClassElement(className) {
 
     for (let i=0; i<elements.length; i++) {
         elements[i].style.display = currState ? "block" : "none"
-        elements[i].popover({
-            title: "Title",
-            content: "Content",
-            container: 'body'
-        });
     }
 }
 
-function getVerticalScrollPercentage( elm ){
-    var p = elm.parentNode
-    return (elm.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight )
-  }
+function getVerticalScrollPercentage(elm) {
+    let p = elm.parentNode
+    let volume = (elm.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight )
+    return volume > 1 ? 1 : volume 
+}
 
 function checkAudioTypes(audioArray) {
     for (let i=0; i<audioArray.length; i++) {
@@ -28,10 +25,10 @@ function checkAudioTypes(audioArray) {
     return true
 }
 
-function setModAudioVolume(scrollSize) {
+function setModAudioVolumeByScroll() {
     ALL_AUDIOS.forEach(audioContainer => {
-        audioContainer["audio"][0].volume = scrollSize;
-        audioContainer["audio"][1].volume = 1 - scrollSize;
+        audioContainer["audio"][0].volume = SCROLL_POSITION;
+        audioContainer["audio"][1].volume = 1 - SCROLL_POSITION;
     });
 }
 
@@ -47,9 +44,7 @@ function playSound(soundId) {
             ],
             "is_playing": false,
         }
-        // TODO we play both sounds normal volume at start and change it on scroll, 
-        //   so the audio is not muted until user scrolls. What's the fix here?
-        // ALL_AUDIOS[soundIndex]["audio"][1].volume = 0;
+        ALL_AUDIOS[soundIndex]
     }
 
     if (!checkAudioTypes(ALL_AUDIOS[soundIndex]["audio"])) {
@@ -63,8 +58,10 @@ function playSound(soundId) {
         })
         ALL_AUDIOS[soundIndex]["is_playing"] = false
     }
+
     else {
         ALL_AUDIOS[soundIndex]["audio"].forEach(audioObj => {
+            setModAudioVolumeByScroll()
             audioObj.loop = true
             audioObj.play()
         })
@@ -74,13 +71,7 @@ function playSound(soundId) {
     document.getElementsByClassName(`piece${soundId}`)[0].classList.toggle(`piece-active`)
 }
 
-function getScrollRGBBlackToRed() {
-    return `rgb(${parseInt(getVerticalScrollPercentage(document.body) * 255)}, 0, 0)`
-}
-
 window.addEventListener('scroll', function () {
-    setModAudioVolume(getVerticalScrollPercentage(document.body))
-    document.getElementsByClassName('svgContainer')[0].style.borderColor = getScrollRGBBlackToRed()
-    // document.body.style.background = `linear-gradient(rgb(255, 255, 255), ${getScrollRGBBlackToRed()});`
-  });
-  
+    SCROLL_POSITION = getVerticalScrollPercentage(document.body)
+    setModAudioVolumeByScroll()
+});
