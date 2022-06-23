d1add85c614d9719b541efef30916d61e46bed86
ehhhahah <ehhhahah@gmail.com>
2022-06-23 23:59:15 +0200

    script update

--- nowa-huta/maps/helpers/inkscape_organizer.py
+++ nowa-huta/maps/helpers/inkscape_organizer.py
@@ -2,24 +2,27 @@ sound_num = 1
 # all_sounds = 11  # counting from zero
 
 color_match = 'style="fill:#'
-color_main = '#ff0000'
-
 new_file_str = ""
+MAIN_COLOR = "#ff0000"
 
-with open('maps/mapa_inkscaped.svg') as map_file:
+with open('nowa-huta/maps/mapa_inkscaped_manual_cut.svg') as map_file:
     for line in map_file:
-        # TODO line = line.replace('fill:#000000', "")
         if "<g" in line:
-            # TODO if sound_num == 1: sound_num += 1 continue
+            if sound_num == 1: 
+                sound_num += 1
+                new_file_str += line
+                continue
 
             line = line.replace(
                 '<g',
                 f"""<g class="puzzle piece{sound_num}" onclick="playSound('nowa-huta/', {sound_num})" """
                 )
             sound_num += 1
+        else:
+            line = line.replace("display:inline;fill:none", f"display:inline;fill:{MAIN_COLOR}")
         new_file_str += line
 
 print(f"Generated {sound_num} pieces")
 
-with open('maps/helpers/new_map_parsed.svg', 'w') as f:
+with open('nowa-huta/maps/helpers/new_map_parsed.svg', 'w') as f:
     f.write(new_file_str)
